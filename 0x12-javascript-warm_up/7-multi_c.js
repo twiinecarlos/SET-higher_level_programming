07857257d8a75e9b3db6d8b6daa314b2ed491e21
Twiine Carlos <twiinecarlos@gmail.com>
2026-09-05 12:40:46 +0200

    Add script to print C is fun multiple times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const occurrences = Number(process.argv[2]);
+
+if (Number.isNaN(occurrences)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+
+  while (i < occurrences) {
+    console.log('C is fun');
+    i++;
+  }
+}
