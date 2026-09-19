0a55a869be879b7c585cca61d44961c80196926e
Twiine Carlos <twiinecarlos@gmail.com>
2026-09-19 12:44:39 +0200

    Add Rectangle validation

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
+      return;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
